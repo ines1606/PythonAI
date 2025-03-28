600ba137a1e4e83b4313b3677e6d788352a99085
ines1606 <169066427+ines1606@users.noreply.github.com>
2025-03-28 13:26:00 +0100

    temperature and max_prompts now adjustable

--- ConversationManager.py
+++ ConversationManager.py
@@ -2,14 +2,29 @@ import os
 from dotenv import load_dotenv
 from together import Together
 
+DEFAULT_TEMPERATURE = 1
+DEFAULT_MAX_TOKENS = 100
+
 class ConversationManager: 
-    def __init__(self, api_key=None, base_url="https://api.together.xyz/v1", persona="sassy"):
+    def __init__(self, api_key=None, base_url="https://api.together.xyz/v1", persona="sassy", temperature=DEFAULT_TEMPERATURE, max_tokens = DEFAULT_MAX_TOKENS):
         load_dotenv() # loads api key from .env file
         self.api_key = api_key or os.getenv("TOGETHER_API_KEY")
         self.base_url = base_url
         self.client = Together()
         self.model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
         self.persona = persona.lower() # convert string to lower-case 
+
+        if 0 <= temperature <= 1:
+            self.temperature = temperature # between 0 and 1, determines degree of randomness 
+        else: 
+            print("Not a valid temperature [0,1], setting it to default value 1")
+            self.temperature = DEFAULT_TEMPERATURE
+
+        if 50 <= max_tokens <= 500:
+            self.max_tokens = max_tokens
+        else: 
+            print("Not a valid amount for tokens [50,500], setting it to default value 100")
+            max_tokens = DEFAULT_MAX_TOKENS
 
         if not self.api_key: 
             raise ValueError("Missing API key!")
@@ -32,5 +47,7 @@ class ConversationManager:
             messages=[{"role":"system", "content": self.system_message},
                       {"role":"user", "content": prompt}],
             stream=False, # api returns answer in one chunk instead of streaming it gradually 
+            temperature=self.temperature,
+            max_tokens=self.max_tokens,
         )
         return response.choices[0].message.content
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# PythonAI
+Creation of a dynamic Chatbot using the together.ai API 
+
+User will be able to change the parameters of temperature, max_tokens and the way the chatbot response, choosing from pre-promted personalities
  
